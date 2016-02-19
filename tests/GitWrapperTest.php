55eef8f4942207f55958c9beff905a78f30f7ff9
Michael Sverdlikovsky <xedelweiss@gmail.com>
2016-02-19 17:41:48 +0200

    Some refactoring

--- tests/GitWrapperTest.php
+++ tests/GitWrapperTest.php
@@ -9,26 +9,32 @@ class GitWrapperTest extends TestCase
 {
     public function testGitInit()
     {
-        $repositoryName = uniqid('repo-');
-        $repositoryPath = $this->getRepositoryPath($repositoryName);
+        $repositoryPath = $this->getRandomRepositoryPath();
 
         $git = Repository::open($repositoryPath, null, 0755, null, false);
+        $commitMsg = 'Add two test files';
 
-        $result = $git->transactional(function(Transaction $t) {
+        $result = $git->transactional(function(Transaction $t) use ($commitMsg) {
             file_put_contents($t->getRepositoryPath().'/text1.txt', 'Test 1');
             file_put_contents($t->getRepositoryPath().'/text2.txt', 'Test 2');
 
-            $t->setCommitMsg('Add two test files');
+            $t->setCommitMsg($commitMsg);
         });
 
         $this->assertTrue(file_exists($repositoryPath));
         $this->assertEmpty($git->getStatus());
         $this->assertCount(1, $git->getLog());
-        $this->assertContains('Add two test files', $git->getLog()[0]);
+        $this->assertContains($commitMsg, $git->getLog()[0]);
     }
 
     protected function getRepositoryPath($repositoryName)
     {
         return $this->getPathToStorageDir($repositoryName);
+    }
+
+    protected function getRandomRepositoryPath()
+    {
+        $repositoryName = uniqid('repo-');
+        return $this->getRepositoryPath($repositoryName);
     }
 }
